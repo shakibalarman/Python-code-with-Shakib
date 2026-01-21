0969d3dc891cf0aef51f6f41bc3acb4863f0be86
shakibalarman <shakibalarman.cse@gmail.com>
2026-01-22 03:06:43 +0600

    practicing nested if else

---
+++ positive-negative.py
@@ -0,0 +1,8 @@
+n = int(input("Enter a number: "))
+if n >= 0:
+    if n == 0:
+        print ("The number is Zero")
+    else:
+        print("The number is positive")
+else:
+    print("The number is negative")
